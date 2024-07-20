8d250e157f30c32f698e31bdd5754a4fa1b0cf6e
Navid Faridi <navid.faridi@outlook.com>
2024-07-20 14:04:07 +0330

    add Generate Random Password endpoint

--- src/Navid.IAM.API/Controllers/v1/AccountController.cs
+++ src/Navid.IAM.API/Controllers/v1/AccountController.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Navid.IAM.API.Controllers.v1;
 
-[Route("api/[controller]")]
+/// <summary>
+/// Account Controller
+/// </summary>
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
 [ApiController]
 public class AccountController : ControllerBase
 {
+    /// <summary>
+    /// Generate Random Password 
+    /// </summary>
+    /// <param name="passwordLength"></param>
+    /// <returns></returns>
+    [HttpGet("PasswordGenerator/{passwordLength}")]
+    [ProducesResponseType(typeof(string), 200)]
+    public IActionResult GenerateRandomPassword(int passwordLength = 8)
+    {
+        var result = Pargoon.Utility.RandomUtils.RandomPassword(passwordLength);
+        return Ok(new { password = result });
+    }
 }
--- src/Navid.IAM.API/Program.cs
+++ src/Navid.IAM.API/Program.cs
@@ -1,19 +1,19 @@
+using Pargoon.ApiVersioning;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+builder.Services.AddSwaggerService(xmlPath);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwaggerVersioned(showSwaggerInAllEnv: true);
 
 app.UseAuthorization();
 
